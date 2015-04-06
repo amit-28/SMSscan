1fbf2757277b811156817b5f301e2193fd924ccb
amit.ja <blueeyeschild@gmail.com>
2015-04-07 00:06:27 +0530

    intialize project

---
+++ app/src/main/java/freeversion/recharge_view/test/smsscan/MainActivity.java
@@ -0,0 +1,51 @@
+package freeversion.recharge_view.test.smsscan;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView view = (TextView)findViewById(R.id.textview1);
+        Uri uriSMSURI = Uri.parse("content://sms/inbox");
+        Cursor cur = getContentResolver().query(uriSMSURI, null, null, null,null);
+        String sms = "";
+        while (cur.moveToNext()) {
+            sms += "From :" + cur.getString(2) + " : " + cur.getString(11)+"\n";
+        }
+        view.setText(sms);
+        setContentView(view);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
